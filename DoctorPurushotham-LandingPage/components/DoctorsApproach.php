dc29d8c18e1fb68d77da02a9cf84aab2a0dea4a6
Lavan Adicherla <143772951+lavankumara2015@users.noreply.github.com>
2024-03-30 14:31:27 +0530

    changes

--- DoctorPurushotham-LandingPage/components/DoctorsApproach.php
+++ DoctorPurushotham-LandingPage/components/DoctorsApproach.php
@@ -1,46 +1,43 @@
-
 <h1 class="doctors-approach-h1">Doctor’s<span> Approach to Treatment</span></h1>
 
 <section>
 
-<div class="doctor__approach__text">
-    <div>
-        <img src="./assets/cancerRibbon.webp" alt="cancerRibbon">
-        
+    <div class="doctor__approach__text">
         <div>
-            <h1>
-                    Dr. Purushottam is extremely talented surgeon known for providing <span> clear treatment options </span> and  <span>  strong moral support </span> to patients and their families.
-            </h1>
+            <img src="./assets/cancerRibbon.webp" alt="cancerRibbon">
+
+            <div>
+                <h1>
+                    Dr. Purushottam is extremely talented surgeon known for providing <span> clear treatment options </span> and <span> strong moral support </span> to patients and their families.
+                </h1>
+            </div>
+        </div>
+        <div>
+            <img src="./assets/cancerRibbon.webp" alt="cancerRibbon">
+            <div>
+                <h1>
+                    Despite his extensive experience, he remains constantly learning and updating himself on the <span> latest techniques </span> in cancer treatment. </h1>
+            </div>
+        </div>
+        <div>
+            <img src="./assets/cancerRibbon.webp" alt="cancerRibbon">
+
+            <div>
+                <h1>
+                    His patients' biggest appreciation has been about how he balances <span>hope and reality in his consultations </span> and ongoing conversations. </h1>
+            </div>
         </div>
     </div>
-<div>
-<img src="./assets/cancerRibbon.webp" alt="cancerRibbon">
-    <div>
-        <h1>
-        Despite his extensive experience, he remains constantly learning and updating himself on the <span> latest techniques </span> in cancer treatment.  </h1>
+    <div class="doctor__approach__form__container">
+        <div><img src="./assets/Phone-icon.webp" alt="phone-logo"> +91 7032656153 / 9989835053</div>
+        <h1 style="text-align: center;">Book Your Appointment</h1>
+        <!-- lead form -->
+        <form action="./components/formRedirectPage.php" method="POST"  id="form__submit">
+            <input type="text" id="name" name="name" placeholder="Name" required pattern="[A-Za-z ]{3,}" minlength="3" maxlength="25" title="Please enter at least 3 alphabetic characters">
+            <input type="tel" id="phone" name="phone" placeholder="Phone Number" required minlength="10" maxlength="14" title="Minimum 10 Numbers Required">
+            <textarea placeholder="Message" id="message" name="message" required minlength="20" maxlength="500" title="Minimum 20 Characters Required"></textarea>
+            <button type="submit">Book Now</button>
+        </form>
     </div>
-</div>
-<div>
-<img src="./assets/cancerRibbon.webp" alt="cancerRibbon">
-   
-   <div>
-       <h1>
-       His patients' biggest appreciation has been about how he balances <span>hope and reality in his consultations </span>  and ongoing conversations. </h1>
-   </div>
-</div>
-</div>
-<div class="doctor__approach__form__container" >
- <div><img src="./assets/Phone-icon.webp" alt="phone-logo"> +91 7032656153 / 9989835053</div>
- <h1 style="text-align: center;">Book Your Appointment</h1>
-
- <form action="./components/formRedirectPage.php" method="POST">
- <input type="text" name="name" placeholder="Name" required pattern="[A-Za-z ]{3,}" minlength="3" maxlength="25" title="Please enter at least 3 alphabetic characters">
- <input type="tel" name="phone" placeholder="Phone Number" required minlength="10" maxlength="14" title="Minimum 10 Numbers Required">
- <textarea placeholder="Message" name="message" required minlength="50" maxlength="500" title="Minimum 50 Characters Required" ></textarea>
- <button text="submit">Book Now</button>
- </form>
-</div>
-
-
 </section>
 
